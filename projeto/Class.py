49b32c6cd02addd9b31d0919133fe3db7d1eea6a
Jandir Palhano <jandirpalhano@gmail.com>
2020-07-13 22:47:14 -0300

    sem alterações

--- projeto/Class.py
+++ projeto/Class.py
@@ -1,19 +1,11 @@
 import requests
 
-
-class EstadoObitos():
+class Estado():
     def __init__(self, uf, definicao):
         lista_estados = ["ac", "al", "am", "ap", "ba", "ce", "df", "es", "go", "ma", "mg", "ms", "mt", "pa", "pb", "pe",
                          "pi", "pr", "rj", "rn", "rs", "ro", "rr", "sc", "se", "sp", "to"]
-        if uf in lista_estados and "Óbitos" in definicao:
-            self.chama_dados_api_obitos(uf)
-        elif uf in lista_estados and "Casos" in definicao:
-            self.chama_dados_api_casos(uf)
-        else:
-            raise ValueError ("UF não disponível na lista de Estados")
-
-    def __str__(self):
-        return self.chama_dados_api_obitos()
+        if not uf in lista_estados:
+            raise ValueError("UF não disponível na lista de Estados")
 
     def chama_dados_api_obitos(self, uf):
         url = requests.get(f"https://covid19-brazil-api.now.sh/api/report/v1/brazil/uf/{uf}")
