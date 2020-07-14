efc41ce6df528a5114b282cb38431e5282a2ad83
Jandir Palhano <jandirpalhano@gmail.com>
2020-07-13 22:52:15 -0300

    Criação das funções multiplicador_por_extenso, definicao_por_extenso, espaco_resultado e lado_superior_inferior_retangulo.

--- projeto/funcion.py
+++ projeto/funcion.py
@@ -1,10 +1,9 @@
-#from Class from
 
 def mensagem_inicial():
     caracteres = 90
     print("=" * caracteres)
     print(" Bem vinda a ferramenta para cálculo da taxa de covid por habitantes no Estados do Brasil ")
-    print("#" * caracteres)
+    print("=" * caracteres)
 
 def definicao_taxa():
     global multiplicador
@@ -13,12 +12,12 @@ def definicao_taxa():
     (2) 500 mil habitantes
     (3) 1 milhão de habitantes
     Selecione a taxa a ser utilizada: """)
-    taxa_por_extenso = ("100 mil habitantes", "500 mil habitantes", "1 milhão de habitantes")
+    lista_taxa_por_extenso = ("100 mil habitantes", "500 mil habitantes", "1 milhão de habitantes")
     lista_opcao_multiplicador = (1, 2, 3)
     lista_opcao_multiplicador = str(lista_opcao_multiplicador)
     while not opcao_multiplicador in lista_opcao_multiplicador:
-        opcao_multiplicador = input("   Digite 1, 2 ou 3: ")
-    print(f"\nA taxa escolhida é de: {taxa_por_extenso[int(opcao_multiplicador) - 1]}\n")
+        opcao_multiplicador = input("    Digite 1, 2 ou 3: ")
+    print(f"\nA taxa escolhida é de: {lista_taxa_por_extenso[int(opcao_multiplicador) - 1]}\n")
     if (opcao_multiplicador == "1"):
         multiplicador = 100000
     elif (opcao_multiplicador == "2"):
@@ -26,6 +25,16 @@ def definicao_taxa():
     elif (opcao_multiplicador == "3"):
         multiplicador = 1000000
     return multiplicador
+
+def multiplicador_por_extenso(multiplicador):
+    lista_taxa_por_extenso = ("100 mil habitantes", "500 mil habitantes", "1 milhão de habitantes")
+    if multiplicador == 100000:
+        taxa_por_extenso = (lista_taxa_por_extenso[0])
+    elif multiplicador == 500000:
+        taxa_por_extenso = (lista_taxa_por_extenso[1])
+    elif multiplicador == 1000000:
+        taxa_por_extenso = (lista_taxa_por_extenso[2])
+    return taxa_por_extenso
 
 
 def definicao_obito_ou_caso():
@@ -47,7 +56,28 @@ def definicao_obito_ou_caso():
         definicao = "Casos"
     return definicao
 
+def definicao_por_extenso(definicao):
+    lista_texto_por_extenso = ("Óbitos", "Casos")
+    if definicao == "Óbitos":
+        definicao_por_extenso = (lista_texto_por_extenso[0])
+    else:
+        definicao_por_extenso = (lista_texto_por_extenso[1])
+    return definicao_por_extenso
+
+
 def calculo_por_habitantes (dados, pop, multiplicador):
     arredondamento = 1
     return round((dados / pop) * multiplicador, arredondamento)
 
+
+def espaco_resultado(resultado):
+    resultado = str(resultado)
+    caracteres_resultado = len(resultado)
+    caracteres_apos_barra = 7
+    caracteres_espaco = caracteres_apos_barra - caracteres_resultado
+    return " " * caracteres_espaco
+
+def lado_superior_inferior_retangulo():
+    barra_horizontal = "-"
+    qtd_caracteres = 18
+    return barra_horizontal * qtd_caracteres
